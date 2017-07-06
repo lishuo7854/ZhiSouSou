ee845503f4755622c128e624a31b2b9d99b42844
lishuo7854 <785483524@qq.com>
2017-07-06 17:11:28 +0800

    tui

--- ZhiSouSou/app/controllers/qi_yejiankong_controller.rb
+++ ZhiSouSou/app/controllers/qi_yejiankong_controller.rb
@@ -1,2 +1,6 @@
 class QiYejiankongController < ApplicationController
+  def index
+    
+  end
+  
 end
--- ZhiSouSou/app/controllers/she_baojiankong_controller.rb
+++ ZhiSouSou/app/controllers/she_baojiankong_controller.rb
@@ -1,4 +1,6 @@
 class SheBaojiankongController < ApplicationController
   def index
   end
+
+
 end
